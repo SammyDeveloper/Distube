2104c3c9a03cb41e5acdd8879d83869e5260a406
Sammy <93799073+SammyDeveloper@users.noreply.github.com>
2021-11-05 17:51:00 -0500

    Update play.js

--- comandos/Música/play.js
+++ comandos/Música/play.js
@@ -7,6 +7,7 @@ module.exports = {
   description : 'Reproduce una canción o una playlist.',
   run: async (client, message, args) => {
 
+    console.log("Comando hecho por Sammy.#3359")
     if(!message.member.voice.channel) return message.reply(':x: `|` Debes estar a un canal de voz para usar este comando')
       
     if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(":x: `|` Debes estar en el mismo canal de voz que yo.")
